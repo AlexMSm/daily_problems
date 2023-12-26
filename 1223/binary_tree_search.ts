f2e3703b5b0e4b4983fb5ab85f02f5147fc2591d
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-12-26 15:31:40 +0000

    Set up binary tree search

---
+++ 1223/binary_tree_search.ts
@@ -0,0 +1,8 @@
+// Binary tree structure is a way of storing data that resembles a tree.
+// All data points are called nodes, the top one is the root node, vertically, there is a parent-child node relationship.
+// Leaf nodes have no children.
+// A binary tree only has two child branches from a node.
+// They are ordered, each left subtree is <= parent node, right subtree is >= parent node.
+// Speeds up searches and subsequent action on an item of data don't have to go through at least half of the tree - takes time proportional to the log of the number of items.
+
+
