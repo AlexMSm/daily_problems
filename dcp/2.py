a50c06500c7d0c2348826a89ae764070d86598c8
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2024-01-04 22:57:40 +0000

    problem 2 fix

--- dcp/2.py
+++ dcp/2.py
@@ -7,9 +7,9 @@
 # If our input was [3, 2, 1], 
 # the expected output would be [2, 3, 6].
 
-list = [1, 2, 3, 4, 5]
+list = [3, 2, 1]
 x = 0
-length = len(list) - 1
+length = len(list)
 index_list = [x for x in range(0, length)]
 new_list = []
 
@@ -17,8 +17,8 @@ for i in list:
     num = 1
     for j in index_list:
         if j != x:
-            num *= list[i]
-    new_list[x] = num
+            num *= list[j]
+    new_list.append(num)
     x += 1
 
 print(new_list)
