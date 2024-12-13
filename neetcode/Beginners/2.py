6dff41cfecd71c23ca34141dd09ae121dc93546b
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2024-12-13 08:09:38 +0000

    Num 2 completed

--- neetcode/Beginners/2.py
+++ neetcode/Beginners/2.py
@@ -6,20 +6,32 @@ class Solution:
             '[': ']'
         }
         order = ['']
-        print(s)
         for letter in s:
-            print('char')
-            print(letter)
-            print('order')
-            print(order)
             if letter not in brackets.keys() and letter != order[-1]:
                 return False
             if letter == order[-1]:
-                print('a')
                 order.pop()
             elif brackets[letter]:
-                print('b')
                 order.append(brackets[letter])
             else:
                 return False
+        if len(order) != 1:
+            return False
         return True
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
